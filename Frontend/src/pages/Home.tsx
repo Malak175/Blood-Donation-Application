6a1f4d0cd6cca6c772347d6d5e7f318dd7fd14d7
Malak Mahmoud <malkzaid2004@gmail.com>
2025-11-01 16:28:48 +0200

    Project

---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import Contact from "../components/Contact";
+import Featured from "../components/Featured";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Element } from "react-scroll";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Element name="hero">
+        <Hero />
+      </Element>
+
+      <Element name="featured">
+        <Featured />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
